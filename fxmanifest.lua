040be8627731d5b9e7ddcb65cea549ff31795181
Tinx69iq <166938947+Tinx69iq@users.noreply.github.com>
2024-12-12 00:05:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ALB-Developments'
+
+description 'A fivem script to disable Desync/Ghost Peeking.'
+
+client_scripts {
+    'Client.lua',
+    'Config.lua'
+}
